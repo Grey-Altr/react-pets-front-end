57b3a92ae81360a0375c310c97367ddd873f48b5
GreyA <grey.altaer@gmail.com>
2025-03-12 09:27:08 -0700

    index complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,33 @@
+import { useState, useEffect } from "react";
+import * as petService from './services/petService.js';
+import PetList from "./components/PetList/PetList.jsx";
+
 const App = () => {
-  return <h1>Hello world!</h1>;
+  const [pets, setPets] = useState([]);
+
+  useEffect(() => {
+    const fetchPets = async () => {
+      try {
+        const fetchedPets = await petService.index();
+        
+        if (fetchedPets.err) {
+          throw new Error(fetchedPets.err);
+        };
+
+        setPets(fetchedPets);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    fetchPets();
+  }, []);
+  
+  return (
+    <>
+      <PetList pets={pets} />
+    </>
+  );
 };
 
 export default App;
