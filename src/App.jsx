6127e0efa67b99d39f3fa7c3350e2b23ab110a53
GreyA <grey.altaer@gmail.com>
2025-03-12 12:08:21 -0700

    CREATE functionality complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
 import { useState, useEffect } from "react";
 import * as petService from './services/petService.js';
 import PetList from "./components/PetList/PetList.jsx";
+import PetDetail from './components/PetDetail/PetDetail.jsx';
+import PetForm from "./components/PetForm/PerForm.jsx";
 
 const App = () => {
   const [pets, setPets] = useState([]);
+  const [selected, setSelected] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
+  const handleSelect = (pet) => {
+    setSelected(pet);
+    setIsFormOpen(false);
+  };
+
+  const handleFormView = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
+  const handleAddPet = async (formData) => {
+    try {
+      const newPet = await petService.create(formData);
+
+      if (newPet.err) {
+        throw new Error(newPet.err);
+      }
+
+      setPets([newPet, ...pets]);
+      setIsFormOpen(false);
+    } catch (err) {
+      console.log(err);
+    };
+  };
 
   useEffect(() => {
     const fetchPets = async () => {
@@ -25,7 +53,17 @@ const App = () => {
   
   return (
     <>
-      <PetList pets={pets} />
+      <PetList
+      pets={pets}
+      handleSelect={handleSelect}
+      handleFormView={handleFormView}
+      isFormOpen={isFormOpen}
+      />
+      {isFormOpen ? (
+        <PetForm handleAddPet={handleAddPet} />
+      ) : (
+        <PetDetail selected={selected} />
+      )}
     </>
   );
 };
